33a2c00be9a7762a38f1414a215f4c5761dc8789
PujaChowdhury <PujaChowdhury@PSDLAP73>
2025-06-10 14:17:04 +0530

    Test suite

---
+++ Test Suites/demo-suite/git-suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>git-suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cfae7437-aacd-4a23-8efd-bf3887543b5a</testSuiteGuid>
+</TestSuiteEntity>
